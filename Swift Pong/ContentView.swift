850af8413bcf543127a88f8f2c1c9d2d0cb6c414
sebdev16 <sebapplelife@gmail.com>
2026-05-19 00:48:22 -0600

    Initial Commit

---
+++ Swift Pong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift Pong
+//
+//  Created by Sebastián Jasso  on 19/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift Pong/Swift_PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PongApp.swift
+//  Swift Pong
+//
+//  Created by Sebastián Jasso  on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
